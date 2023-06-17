cb1a5691f9f8fee6c60aa5ea05c9035a75e2deab
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-06-17 16:06:00 -0300

    minor style changes at contact page

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -7,25 +7,25 @@ import "./Contacto.css"
 const Contacto = () => {
   return (
     <div className="contact-background">
-        <h1 className="contact-title">Contactanos para realizar tu consulta o reserva!</h1>
+        <h1 className="contact-title">Contactenos para realizar su consulta o reserva!</h1>
         <Container className="contact-page-container">
             <Row className="row-container">
                 <Col className="contact-banner">
                 </Col>
                 <Col className="contact-container">
                     <Row>
-                        <h3>Nuestras redes</h3>
-                        <p>En la brevedad estaremos respondiendo tu consulta</p>
+                        <h3 className="fs-2 pb-3">Nuestras redes</h3>
+                        <p className="fs-4 pb-5">En la brevedad estaremos respondiendo su consulta</p>
                     </Row>
                     <Row>
-                        <ul className="list-items">
+                        <ul className="contact-list-items">
                             <li>
                                 <img src={whatsappLogo} className="whatsapp-logo" alt="whatsapp"/> +54 9 11 5555 5555
                             </li>
                             <li>
                                 <Link to='https://www.instagram.com/ignacioarresesom/' className="instagram-link contact-link">
                                     <Row className="instagram-logo-container">
-                                        <Col className="instagram-logo-col"><img src={instagramLogo} className="instagram-logo-contacto" alt="instagram"/></Col> <Col>@ignacioarresesom</Col>
+                                        <Col className="instagram-logo-col"><img src={instagramLogo} className="instagram-logo-contacto" alt="instagram"/></Col> <Col className="p-0">@ignacioarresesom</Col>
                                     </Row>
                                 </Link> 
                             </li>
