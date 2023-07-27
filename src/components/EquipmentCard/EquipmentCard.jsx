7754c823be6f90f27edd9962e7a8b6bc50017b39
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-07-27 14:15:46 -0300

    new equipmentCard style and corrected responsiveness at home page

--- src/components/EquipmentCard/EquipmentCard.jsx
+++ src/components/EquipmentCard/EquipmentCard.jsx
@@ -1,17 +1,19 @@
-import { Button, Card } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 import "./EquipmentCard.css"
 import { Link } from "react-router-dom"
 
 const EquipmentCard = ({item}) => {
   return (
-    <Card style={{ width: '18rem' }} className="my-3">
-      <Card.Img variant="top" src={item.images[0]?.image_url} />
-      <Card.Body className="d-flex flex-column justify-content-end">
-        <Card.Title>{item.name}</Card.Title>
-        <Card.Subtitle className="pb-3">Precio x dia: $ {item.price}</Card.Subtitle>
-        <Link to={`/detalle/${item.id}`}><Button variant="dark">Ver disponibilidad</Button></Link>
-      </Card.Body>
-    </Card>
+    
+      <Card style={{ width: '18rem' }} className="my-3 equipment-card">
+        <Link to={`/detalle/${item.id}`} className="text-decoration-none text-dark h-100 d-flex flex-column justify-content-between">
+        <Card.Img variant="top" src={item.images[0]?.image_url} />
+        <Card.Body className="d-flex flex-column justify-content-end">
+          <Card.Title>{item.name}</Card.Title>
+          <Card.Subtitle className="pb-3">Precio por dia: $ {item.price}</Card.Subtitle>
+        </Card.Body>
+        </Link>
+      </Card>
   )
 }
 export default EquipmentCard
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,8 +24,8 @@ const Home = () => {
     <div className="page-container">
       <HomeCarousel />
       <Container className='p-0 container-custom-style'>
-        <Row><h1 className='text-start mt-5 mx-5'>Nuestros equipos.</h1></Row>
-        <Row className="justify-content-between py-4 px-5">
+        <Row className='custom-row'><h1 className='text-start m-5 pb-5 w-100 home-title'>Nuestros equipos.</h1></Row>
+        <Row className="justify-content-between py-4 px-5 custom-row">
           { 
             items.map((item, i) => {
               return <EquipmentCard key={i} item={item}/>
