c53d83a8f43617781c1887ae46f3ddf0739266d1
RamiroAlvarezAbelleira <alvarezramiro1997@gmail.com>
2023-07-28 19:40:10 -0300

    Header component responsivness updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,13 +14,13 @@ const Header = () => {
 
     /* active and inactive link styles*/
 
-    let active = 'p-3 text-center text-decoration-none text-white active-link'
-    let notActive = 'p-3 text-center text-decoration-none inactive-link'
+    let active = 'text-center text-decoration-none text-white active-link'
+    let notActive = 'text-center text-decoration-none inactive-link'
 
   return (
     <Navbar expand="md" className='header'>
       <Navbar.Brand href="/"><NavLink to="/"><img src={image} alt="BiciMundo" width="200" className='home-logo'/></NavLink></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav d-flex">
         <Nav className='nav-container'>
           <Container className='header-container'>
@@ -37,42 +37,10 @@ const Header = () => {
             </Form>
           </Row>
           <Row className='links-row'>
-            <Col className='mt-2 d-flex align-items-center'>
+            <Col className='mt-2 d-flex align-items-start'>
               <NavLink to="/" end className={({ isActive }) => (isActive ? active : notActive)}  >
                 Home
               </NavLink>
-                <NavDropdown
-                      id="nav-dropdown-dark-example"
-                      align="end"
-                      title='Equipos'
-                      className='px-1 py-0 m-0 rent-dropdown'
-                    >
-                      <Dropdown.Item>
-                        <NavLink to="/perfil" className="text-decoration-none text-dark">
-                          Raquetas de nieve
-                        </NavLink>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <NavLink to="/carrito" className="text-decoration-none text-dark">
-                          Mochilas
-                        </NavLink>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <NavLink to="/carrito" className="text-decoration-none text-dark">
-                          Carpas
-                        </NavLink>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <NavLink to="/carrito" className="text-decoration-none text-dark">
-                          Bolsas de dormir
-                        </NavLink>
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                        <NavLink to="/carrito" className="text-decoration-none text-dark">
-                          Bastones de trekking
-                        </NavLink>
-                      </Dropdown.Item>
-                    </NavDropdown>
             </Col>
             <Col className='right-links-container mt-2'>
                 <NavLink to="/nosotros" className={({ isActive }) => (isActive ? active : notActive)} >
